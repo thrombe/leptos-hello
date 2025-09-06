ed05ef7d59df1f71ce18d3d2272838380380db78
Saikat Das <saikatdas0790@gmail.com>
2025-09-06 19:42:17 +0400

    Change greeting message in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use leptos::prelude::*;
 
 fn main() {
-    leptos::mount::mount_to_body(|| view! { <p>"Hello, everyone!"</p> })
+    leptos::mount::mount_to_body(|| view! { <p>"Hello, Yral!"</p> })
 }
